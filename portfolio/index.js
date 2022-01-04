5c72938bf42cc124dc8c98ed4b200578fd9ca076
Hanna Papova <seal.annuska@gmail.com>
2022-01-04 16:36:42 -0300

    feat: add button and links handler js

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,15 @@
-// code comes here
+const handleClick = (event) => {
+  let element = event.target;
+
+  if (element.classList.contains('button--submit') || element.classList.contains('icons')) {
+    preventDefault(event);
+  }
+};
+
+const preventDefault = (event) => {
+  event.preventDefault();
+};
+
+if (document.addEventListener) {
+  document.addEventListener('click', handleClick, false);
+}
