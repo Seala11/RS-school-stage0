73bb0b2158ae26316cce5ec8a119bb663d95b0f6
Hanna Papova <seal.annuska@gmail.com>
2022-01-20 15:13:21 -0300

    feat: update volume controls

--- custom-video/index.js
+++ custom-video/index.js
@@ -3,13 +3,19 @@ import showTask from "./task.js";
 const player = document.querySelector('.player');
 const video = player.querySelector('.player__video');
 const controlsPlayBtn = player.querySelector('.controls__play');
-const volume = player.querySelector('.controls__volume');
+const volume = player.querySelector('.volume');
+const volumeIcon = player.querySelector('.volume__icon');
 const currTimeElement = player.querySelector('.time__current');
 const durationTimeElement = player.querySelector('.time__duration');
 
+let currentVolume = 0.15;
+let videoIsMuted = false;
+
 const togglePlay = (event) => {
+   console.log(videoIsMuted)
    if (video.paused) {
       video.play();
+      setVideoVolume()
    } else {
       video.pause();
    }
@@ -21,6 +27,7 @@ const playBtnToggle = () => {
    if (controlsPlayBtn.classList.contains('play-btn')) {
       controlsPlayBtn.classList.remove('play-btn');
       controlsPlayBtn.classList.add('pause-btn');
+      setVideoVolume();
    } else {
       controlsPlayBtn.classList.remove('pause-btn');
       controlsPlayBtn.classList.add('play-btn');
@@ -28,8 +35,32 @@ const playBtnToggle = () => {
 }
 
 //Volume
+// TODO: on hover if volume muted turnes on the volume (shouldnt)
+const setVideoVolume = () => {
+   (videoIsMuted === 'true') ? video.volume = 0 : video.volume = currentVolume;
+}
+
 const volumeHandler = (event) => {
    video.volume = event.target.value;
+   if (video.volume === 0) {
+      videoIsMuted = true;
+      volumeMuteHandler();
+   }
+   currentVolume = video.volume;
+};
+
+const volumeMuteHandler = () => {
+   if (volumeIcon.classList.contains('volume-on-btn')) {
+      volumeIcon.classList.remove('volume-on-btn');
+      volumeIcon.classList.add('volume-mute-btn')
+      video.volume = 0;
+      videoIsMuted = 'true';
+   } else {
+      volumeIcon.classList.remove('volume-mute-btn')
+      volumeIcon.classList.add('volume-on-btn');
+      video.volume = currentVolume;
+      videoIsMuted = 'false';
+   }
 }
 
 //Current time and duration
@@ -43,8 +74,10 @@ const currentTime = () => {
    durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds < 10 ? '0' + durationSeconds : durationSeconds}`;
 }
 
+video.addEventListener('click', togglePlay);
 controlsPlayBtn.addEventListener('click', togglePlay);
 volume.addEventListener('mousemove', volumeHandler);
+volumeIcon.addEventListener('click', volumeMuteHandler);
 video.addEventListener('timeupdate', currentTime);
 
-showTask();
+// showTask();
