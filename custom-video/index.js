68b13a86c37059740a59ff1c25280400c4982302
Hanna Papova <seal.annuska@gmail.com>
2022-01-19 22:52:34 -0300

    feat: add duration function

--- custom-video/index.js
+++ custom-video/index.js
@@ -2,7 +2,10 @@ import showTask from "./task.js";
 
 const player = document.querySelector('.player');
 const video = player.querySelector('.player__video');
-const controlsPlayBtn = player.querySelector('.controls__play')
+const controlsPlayBtn = player.querySelector('.controls__play');
+const volume = player.querySelector('.controls__volume');
+const currTimeElement = player.querySelector('.time__current');
+const durationTimeElement = player.querySelector('.time__duration');
 
 const togglePlay = (event) => {
    if (video.paused) {
@@ -25,7 +28,23 @@ const playBtnToggle = () => {
 }
 
 //Volume
+const volumeHandler = (event) => {
+   video.volume = event.target.value;
+}
+
+//Current time and duration
+const currentTime = () => {
+   let currentMinutes = Math.floor(video.currentTime / 60);
+   let currentSeconds = Math.floor(video.currentTime - currentMinutes * 60);
+   let durationMinutes = Math.floor(video.duration / 60);
+   let durationSeconds = Math.floor(video.duration - durationMinutes * 60);
+   
+   currTimeElement.innerHTML = `${currentMinutes}:${currentSeconds < 10 ? '0' + currentSeconds : currentSeconds}`;
+   durationTimeElement.innerHTML = `${durationMinutes}:${durationSeconds < 10 ? '0' + durationSeconds : durationSeconds}`;
+}
 
 controlsPlayBtn.addEventListener('click', togglePlay);
+volume.addEventListener('mousemove', volumeHandler);
+video.addEventListener('timeupdate', currentTime);
 
 showTask();
