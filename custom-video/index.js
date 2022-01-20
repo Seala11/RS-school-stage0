138820f90c23f6fe315f540cf052d1c617b76ad1
Hanna Papova <seal.annuska@gmail.com>
2022-01-19 21:38:31 -0300

    feat: add video section play button

--- custom-video/index.js
+++ custom-video/index.js
@@ -1,6 +1,27 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.player__video');
+const controlsPlayBtn = player.querySelector('.controls__play')
 
+const togglePlay = (event) => {
+   if (video.paused) {
+      video.play();
+   } else {
+      video.pause();
+   }
+   playBtnToggle();
+}
 
+const playBtnToggle = () => {
+   if (controlsPlayBtn.classList.contains('play-btn')) {
+      controlsPlayBtn.classList.remove('play-btn');
+      controlsPlayBtn.classList.add('pause-btn');
+   } else {
+      controlsPlayBtn.classList.remove('pause-btn');
+      controlsPlayBtn.classList.add('play-btn');
+   }
+}
 
+controlsPlayBtn.addEventListener('click', togglePlay);
 
 const task = `
 ## Требования (60 /65)
