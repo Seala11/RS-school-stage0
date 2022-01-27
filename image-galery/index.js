9bf28be3c59a25049f1d0d48a998411947cebc0b
Hanna Papova <seal.annuska@gmail.com>
2022-01-27 13:23:16 -0300

    feat: add load Image function

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,3 +1,49 @@
-import showTask from "./task.js";
+import showTask from './task.js';
 
-showTask();
+const userInput = document.getElementById('search');
+const mainSection = document.querySelector('.main');
+
+const inputHandler = (event) => {
+  if (event.key === 'Enter') loadImages();
+};
+
+const loadImages = async () => {
+  clearMainSection();
+
+  let searchKey = userInput.value;
+
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?query=${searchKey}&client_id=IzymClZpgZ906XDwWPSawOkxCO6lu0NrULHFhjj9KFc`
+    );
+    console.log(response);
+
+    // if the response is correct:
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      console.log(data.urls.regular)
+
+      mainSection.innerHTML = `<img src=${data.urls.regular}></img>`;
+    } else if(response.status === 401){
+        console.log('The key is wrong');
+    } else if(response.status === 404){
+        console.log('The image that you are looking for does not exist');
+    } else {
+        console.log('Something went wrong');
+    }
+  } catch (error) {
+    // if 404 => {errors: Array(1)}
+    // errors: ['No photos found.']
+    // [[Prototype]]: Object
+    console.log(error);
+  }
+};
+
+const clearMainSection = () => {
+  mainSection.innerHTML = '';
+};
+
+// loadImages();
+userInput.addEventListener('keydown', inputHandler);
+// showTask();
