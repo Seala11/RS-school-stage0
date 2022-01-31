c3fa59d61e7de297c27466b3abba059074901960
Hanna Papova <seal.annuska@gmail.com>
2022-01-31 14:44:46 -0300

    refactor: js files

--- image-galery/index.js
+++ image-galery/index.js
@@ -21,13 +21,14 @@ const ERROR_MESSAGE = {
 
 const ERROR = {
   401: ' Invalid access key',
-  403: ' For applications in demo mode, the Unsplash API currently places a limit of 50 requests per hour. Looks like you are out of limit. Try again in one hour.',
+  403: ' For applications in demo mode, the Unsplash API currently places a limit of 50 requests per hour. Looks like you have reached the limit. Try again in one hour.',
   404: ' The image that you are looking for does not found',
   else: 'Something went wrong, status code: ',
 };
 
 let inputData = []; // to show-hide clear btn in clearBtnToggle
 
+// search bar handler
 const inputHandler = (event) => {
   if (event.key === 'Enter') searchHandler();
 };
@@ -49,7 +50,6 @@ const makeRequest = async (searchKey) => {
 
     if (status === 200) {
       const data = await response.json();
-      console.log(data);
       if (data.total === 0) showError('', ERROR_MESSAGE['404']);
       loadImages(data);
     } else if (status === 401) {
@@ -75,41 +75,57 @@ const loadImages = (data) => {
     let image = document.createElement('div');
     image.className = 'main__img';
     image.style.backgroundImage = `url(${element.urls.raw}&w=1366&h=768)`;
-    image.addEventListener('click', () => imageOpen(element));
+    image.addEventListener('click', () => modalHandler(element));
     sectionGalery.appendChild(image);
   });
 };
 
 // modal handler
-const imageOpen = (element) => {
-  console.log(element);
+const modalHandler = (element) => {
   modal.classList.add('modal__open');
   sectionGalery.classList.add('block-hidden');
+
   // set image inside modal
+  modalImageHandler(element);
+
+  //add image description (modal footer)
+  modalDescHandler(element);
+
+  // add link to download btn (will open the pict in new tab);
+  modalDownloadBtn.href = element.links.download;
+};
+
+const modalImageHandler = (element) => {
   const imgEl = document.querySelector('.modal__image');
   imgEl.src = `${element.urls.raw}&w=1366&h=768`;
   imgEl.alt = element.alt_description;
-  imgEl.width = `${element.width}`;
-  imgEl.height = element.height * 0.2;
+  imgEl.width = element.width;
+  imgEl.height = element.height;
   modal.appendChild(imgEl);
+};
 
-  //add modal description
+const modalDescHandler = (element) => {
   const descripEl = document.querySelector('.modal__desc');
   const userName = descripEl.querySelector('.desc_user');
   const userImg = descripEl.querySelector('.user_img');
   const descPhoto = descripEl.querySelector('.desc_photo');
 
-  element.user.name === null ? userName.innerText = '' : userName.innerText = element.user.name
+  // add photographer name if exist
+  element.user.name === null
+    ? (userName.innerText = '')
+    : (userName.innerText = element.user.name);
   userName.href = element.user.links.html;
-  element.user.profile_image.medium === null ? userImg.style.backgroundImage = `url(./assets/svg/no-image.jpg)` : userImg.style.backgroundImage = `url(${element.user.profile_image.medium})`;
-  userImg.style.backgroundImage = `url(${element.user.profile_image.medium})`;
-  element.description === null ? descPhoto.innerText = '' : descPhoto.innerText = `"${element.description}"`;
 
-  // add download btn
-  modalDownloadBtn.href = element.links.download;
-  modalDownloadBtn.addEventListener('click', () => {
-    console.log(modalDownloadBtn.href);
-  });
+  // add photographer image if exist or set deafault image
+  element.user.profile_image.medium === null
+    ? (userImg.style.backgroundImage = `url(./assets/svg/no-image.jpg)`)
+    : (userImg.style.backgroundImage = `url(${element.user.profile_image.medium})`);
+  userImg.style.backgroundImage = `url(${element.user.profile_image.medium})`;
+
+  // add phote description if exist
+  element.description === null
+    ? (descPhoto.innerText = '')
+    : (descPhoto.innerText = `"${element.description}"`);
 };
 
 const closeModal = () => {
@@ -121,11 +137,11 @@ const clearMainSection = () => {
   sectionGalery.innerHTML = '';
 };
 
-// error handling
+// error handler
 const showError = (status, message) => {
   let error = document.createElement('div');
   error.className = 'main__img';
-  error.style.cursor = 'default'
+  error.style.cursor = 'default';
   error.innerHTML = `
     <h1>${status > 0 ? status : ''}</h1>
     <p>${message}</p>
@@ -167,4 +183,4 @@ searchBtn.addEventListener('click', searchHandler);
 clearBtn.addEventListener('click', clearInput);
 modalCloseBtn.addEventListener('click', closeModal);
 
-// showTask();
+showTask();
