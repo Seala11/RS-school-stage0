4e009f53f0cd45713fed41985abf8751735787e0
Hanna Papova <seal.annuska@gmail.com>
2022-02-02 15:47:10 -0300

    feat: add card styles

--- memory-game/index.js
+++ memory-game/index.js
@@ -6,7 +6,7 @@ import showTask from './task.js';
 // 4. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой сохраняются результаты предыдущих 10 игр
 // 5. По клику на карточку – она переворачивается плавно, если пара не совпадает – обе карточки переварачиваются
 
-const gameSection = document.querySelector('.game__section');
+const gameSection = document.querySelector('.grid');
 const playerLivesCounter = document.querySelector('.controls__score-counter');
 let playerLives = 3;
 
@@ -17,23 +17,60 @@ playerLivesCounter.textContent = playerLives;
 // TODO: make 3 levels different module for each data
 // resize images
 // this one is 4 on 4
-const getImages = [
-  {src: '.images/zebra.jpg', name: 'zebra'},
-  {src: '.images/bug2.jpg', name: 'bug'},
-  {src: '.images/cat3.jpg', name: 'cat'},
-  {src: '.images/shark4.jpg', name: 'shark'},
-  {src: '.images/dog5.jpg', name: 'dog'},
-  {src: '.images/yellow3.jpg', name: 'lion'},
-  {src: '.images/white2.jpg', name: 'cat-white'},
-  {src: '.images/red3.jpg', name: 'bird'},
-  {src: '.images/zebra.jpg', name: 'zebra'},
-  {src: '.images/bug2.jpg', name: 'bug'},
-  {src: '.images/cat3.jpg', name: 'cat'},
-  {src: '.images/shark4.jpg', name: 'shark'},
-  {src: '.images/dog5.jpg', name: 'dog'},
-  {src: '.images/yellow3.jpg', name: 'lion'},
-  {src: '.images/white2.jpg', name: 'cat-white'},
-  {src: '.images/red3.jpg', name: 'bird'},
-];
+const getImages = () => ([
+  { src: './assets/images/zebra.jpg', name: 'zebra' },
+  { src: './assets/images/bug2.jpg', name: 'bug' },
+  { src: './assets/images/cat3.jpg', name: 'cat' },
+  { src: './assets/images/shark4.jpg', name: 'shark' },
+  { src: './assets/images/dog5.jpg', name: 'dog' },
+  { src: './assets/images/yellow3.jpg', name: 'lion' },
+  { src: './assets/images/white2.jpg', name: 'cat-white' },
+  { src: './assets/images/red3.jpg', name: 'bird' },
+  { src: './assets/images/zebra.jpg', name: 'zebra' },
+  { src: './assets/images/bug2.jpg', name: 'bug' },
+  { src: './assets/images/cat3.jpg', name: 'cat' },
+  { src: './assets/images/shark4.jpg', name: 'shark' },
+  { src: './assets/images/dog5.jpg', name: 'dog' },
+  { src: './assets/images/yellow3.jpg', name: 'lion' },
+  { src: './assets/images/white2.jpg', name: 'cat-white' },
+  { src: './assets/images/red3.jpg', name: 'bird' },
+]);
+
+const loadRandomData = () => {
+  const images = getImages();
+  images.sort(() => Math.random() - 0.5);
+  return images;
+};
+
+const cardGenerator = () => {
+  const cards = loadRandomData();
+  cards.forEach(card => {
+    // create a card element with face and back;
+    const cardEl = document.createElement('div');
+    const cardFace = document.createElement('div');
+    const cardBack = document.createElement('div');
+    cardEl.classList = 'card';
+    cardFace.classList = 'card-face';
+    cardBack.classList = 'card-back';
+
+    // add img to the card face
+    cardFace.style.backgroundImage = `url(${card.src})`;
+
+    // add card element to the main section
+    gameSection.appendChild(cardEl);
+    cardEl.appendChild(cardFace);
+    cardEl.appendChild(cardBack);
+    
+    // add events to the card
+    cardEl.addEventListener('click', () => cardHandler(card, cardEl));
+  })
+}
+
+const cardHandler = (cardInfo, cardEl) => {
+  console.log(cardInfo.name)
+  cardEl.classList.toggle('card-flip');
+}
+
+cardGenerator();
 
 // showTask();
