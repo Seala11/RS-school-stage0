b7bff36891c522e6d54a8f03f5e2a794970eea47
Hanna Papova <seal.annuska@gmail.com>
2022-02-03 13:54:37 -0300

    feat: add score and moves counter

--- memory-game/index.js
+++ memory-game/index.js
@@ -1,17 +1,13 @@
 import showTask from './task.js';
 // 0. add favicon)
-// 1. Карточки, по которым кликнул игрок, переворачиваются согласно правилам игры
 // 2. Игра завершается, когда открыты все карточки
 // 3. По окончанию игры выводится её результат - количество ходов, которые понадобились для завершения игры
 // 4. Результаты последних 10 игр сохраняются в local storage. Есть таблица рекордов, в которой сохраняются результаты предыдущих 10 игр
 // 5. По клику на карточку – она переворачивается плавно, если пара не совпадает – обе карточки переварачиваются
 
 const gameSection = document.querySelector('.grid');
-const playerLivesCounter = document.querySelector('.controls__score-counter');
-let playerLives = 3;
-
-// ===== show lives in controls section ======
-playerLivesCounter.textContent = playerLives;
+const playerScoreCounter = document.querySelector('.controls__score-counter');
+const playerMovesCounter = document.querySelector('.controls__moves-counter')
 
 // ===== load images to game section =====
 // TODO: make 3 levels different module for each data
@@ -42,7 +38,7 @@ const loadRandomData = () => {
   return images;
 };
 
-// ============= set random cards in the main section ================
+// ============= CARD GENERATOR SECTION ================
 
 const cardGenerator = () => {
   const cards = loadRandomData();
@@ -70,7 +66,7 @@ const cardGenerator = () => {
   });
 };
 
-// ================== card flipping logic ==================
+// ================== CARD FLIPPING LOGIC ==================
 
 let firstCardName; // the first card id to check the match
 let currentCardsId = []; // to check if those are different cards
@@ -102,6 +98,7 @@ const cardHandler = (event) => {
     matched === false
       ? setTimeout(flipCardsBack, 1400) // more time for 2 cards stayed both opened for 800ms
       : setTimeout(removeMatchedCards, 1000); // add 400ms before cards disapppear
+    setTimeout(movesUpdate, 800);
   }
 };
 
@@ -120,14 +117,15 @@ const checkIfMatch = (secondCardName) => {
 const flipCardsBack = () => {
   currentCards.forEach((el) => flipToggle(el));
   console.log('flipback');
-   // after flip back elements, remove chosen elements so the player continue
-   // the timer needed to flip animation be done;
+  // after flip back elements, remove chosen elements so the player continue
+  // the timer needed to flip animation be done;
   setTimeout(clearPlayerChoice, 600);
 };
 
 const removeMatchedCards = () => {
   currentCards.forEach((el) => el.classList.add('fixed'));
   console.log('matched');
+  scoreUpdate();
   // after fixing elements, remove chosen elements so the player continue
   clearPlayerChoice();
 };
@@ -137,6 +135,20 @@ const clearPlayerChoice = () => {
   currentCardsId = [];
   console.log('done');
 };
+
+// ================ SCORE UPDATE & MOVES UPDATES ======================
+let currentScore = 0;
+let currentMoves = 0;
+
+const scoreUpdate = () => {
+  currentScore += 10;
+  playerScoreCounter.textContent = currentScore;
+};
+
+const movesUpdate = () => {
+  currentMoves += 1;
+  playerMovesCounter.textContent = currentMoves;
+}
 
 cardGenerator();
 
